778b92822153718629785ff16a6c0912e5c293de
DilanBars2017 <developeryudintsev@gmail.com>
2025-11-12 17:17:44 +0300

    English_Project

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -8,7 +8,8 @@ import {PracticeComponent} from "./body/smallCamponents/PracticeCamponent";
 import {Box} from "@mui/material";
 import {getQuestions} from "./Data/Data";
 import Stack from '@mui/material/Stack';
-import SvgIcon, {SvgIconProps} from '@mui/material/SvgIcon';
+import SvgIcon from '@mui/material/SvgIcon';
+import type { SvgIconProps } from '@mui/material/SvgIcon';
 import {Link} from "react-router-dom";
 
 export function HomeIcon(props: SvgIconProps) {
