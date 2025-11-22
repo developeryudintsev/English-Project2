2d518b4db33e381cd56732ac5855420303122310
DilanBars2017 <developeryudintsev@gmail.com>
2025-11-22 06:57:05 +0300

    English_Project

--- my-react-app/src/Header/Header.tsx
+++ my-react-app/src/Header/Header.tsx
@@ -17,7 +17,7 @@ import Modal from "@mui/material/Modal";
 import {IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableRow} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import type {DataType, RatingMap} from "../Data/Data";
-import {computeRatingMapFromData, getQuestions, getRatingMap, initDB, setRatingMap} from "../Data/Data";
+import {computeRatingMapFromData, getQuestions, getRatingMap,  setRatingMap} from "../Data/Data";
 
 type HeaderType = {
     time: timeType;
